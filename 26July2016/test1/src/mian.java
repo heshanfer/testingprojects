85beb57de04aed80a67be3052bdd429ed279fe57
heshanf <heshanferndz@hotmail.com>
2016-07-26 15:15:54 +0530

    Commit message

--- 26July2016/test1/src/mian.java
+++ 26July2016/test1/src/mian.java
@@ -5,5 +5,8 @@ public class mian {
 
     public static void main(String[] args) {
         System.out.println("Testing");
+        for (int i=0;i<10;i++){
+            System.out.println(""+i);
+        }
     }
 }
